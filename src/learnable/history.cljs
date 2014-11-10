81d7cb704ad396dd258311dbc017947e523bb3bb
Dmitry Kolobov <dmkolobov@gmail.com>
2014-11-10 02:33:22 -0700

    Display time index in timeline.

--- src/learnable/history.cljs
+++ src/learnable/history.cljs
@@ -15,7 +15,7 @@
                     (cons (dom/li
                             nil
                             (dom/a #js {:onClick (fn [e] (put! control atime))}
-                                   (dom/span #js {:className "time-index"})
+                                   (dom/span #js {:className "time-index"} atime)
                                    (str input)))
                           timeline)))
                 (list)
