933f4c1797a1a8ee219529646ad7f7b0b3f67928
Dmitry Kolobov <dmkolobov@gmail.com>
2014-11-10 18:28:13 -0700

    Remove old history componenet code.

--- src/learnable/history.cljs
+++ src/learnable/history.cljs
@@ -3,25 +3,6 @@
             [om.dom :as dom :include-macros true]
             [cljs.core.async :as async :refer [put!]]))
 
-(defn vcomponent [log owner]
-  (reify
-    om/IRenderState
-    (render-state [_ {:keys [control]}]
-      (apply
-        dom/ul
-        #js {:className "computer-history screen"}
-        (reduce (fn [timeline atime]
-                  (let [input (log atime)]
-                    (cons (dom/li
-                            nil
-                            (dom/a #js {:onClick (fn [e] (put! control atime))}
-                                   (str atime " : "input)))
-                          timeline)))
-                (list)
-                (range (count log)))))))
-
-
-
 (defn vcomponent [process owner]
   (reify
     om/IRenderState
